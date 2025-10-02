4a569f3c60f4932f31bfc5e52976440a950f8ecc
Darranya Koneswaran <darranyakoneswaran@gmail.com>
2025-10-02 09:36:29 +0530

    Implemented login
    Implemented proper login same as the signup implementation

--- react-frontend/src/views/Login.js
+++ react-frontend/src/views/Login.js
@@ -1,21 +1,57 @@
 import {Link} from "react-router-dom";
+import {useRef, useState} from "react";
+import axiosClient from "../axios-client";
+import {useStateContext} from "../contexts/ContextProvider";
 
 export default function Login(){
+
+    const emailRef = useRef();
+    const passwordRef = useRef();
+
+    const [message, setMessage] = useState(null);
+
+    const { setUser, setToken } = useStateContext();
+
     const onSubmit = (ev) => {
-        ev.preventDefault()
+        ev.preventDefault();
 
-    }
+        const payload = {
+            email: emailRef.current.value,
+            password: passwordRef.current.value,
+        };
+
+        setMessage(null); // reset message before new request
+
+        axiosClient.post('/login', payload)
+            .then(({ data }) => {
+                setUser(data.user);
+                setToken(data.token);
+            })
+            .catch(err => {
+                const response = err.response;
+                if (response && response.status === 422) {
+                    setMessage(response.data.message);
+                }
+            });
+    };
+
     return(
+        <form onSubmit={onSubmit}>
+            <h1 className="title">Login into your account</h1>
 
-                <form onSubmit={onSubmit}>
-                    <h1 className="title"> Login into your account</h1>
-                    <input type="email" placeholder="Email"/>
-                    <input type="password" placeholder="Password"/>
-                    <button className="btn btn-block"> Login</button>
-                    <p className="message">
-                        Not Registered? <Link to="/signup">Create an account</Link>
-                    </p>
-                </form>
+            {message &&
+                <div className="alert">
+                    <p>{message}</p>
+                </div>
+            }
 
-    )
+            <input ref={emailRef} type="email" placeholder="Email" />
+            <input ref={passwordRef} type="password" placeholder="Password" />
+            <button className="btn btn-block">Login</button>
+
+            <p className="message">
+                Not Registered? <Link to="/signup">Create an account</Link>
+            </p>
+        </form>
+    );
 }
